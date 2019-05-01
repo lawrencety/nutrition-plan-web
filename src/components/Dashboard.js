c8c7ac1845c9db9aa9088e508cf1c70311393f7a
lawrence <lawrence.t.yen@gmail.com>
2019-05-01 15:37:51 -0700

    home and dashboard skeleton

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
 
-const Dashboard = () => (
-  <section className="dashboard">
-    Dashboard page will go here
-  </section>
-)
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <section className="home">
+        <div className="hero">
+          <Card className="card">
+            <CardContent>
+              <h2>Today's Performance</h2>
+              <Divider />
+              <h3>Overall</h3>
+              <Divider />
+              <h3>Major Nutrients</h3>
+            </CardContent>
+          </Card>
+          <Card className="card">
+            <CardContent>
+              <h1>Good morning</h1>
+              <h1>What did you have for breakfast?</h1>
+            </CardContent>
+            <CardActions>
+              <Button variant="contained" color="primary">Add Foods</Button>
+            </CardActions>
+          </Card>
+        </div>
+      </section>
+    )
+  }
+}
+
 
 export default Dashboard;
